a86fe8fac16d4a394f5596da89a6666ef5861bce
sloraris <97694636+sloraris@users.noreply.github.com>
2024-11-16 21:06:16 -0700

    added initial terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "3.0.1-rc4"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = var.pm_api_url
+  pm_api_token_id = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure = true  # Set to false in production
+}
+
+resource "proxmox_vm_qemu" "ghost" {
+  name = "ghost"
+  target_node = "artemis"
+}
